4da62705afabfc865b7d73632afd7c92a58b15b6
Kaleb Petersen <kcpetersen111@gmail.com>
2022-06-24 10:04:22 -0600

    adding the stubs for the api

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const app = express();
 
-app
+//will need a way to get companies from the db and send it to the front end
+app.get("/getReview",(res,req)=>{
+
+});
+
+//will need a way to post a review of the lunch
+app.post("/postReview",(res,req)=>{
+
+});
+
+//will need a way to delete reviews
+app.delete("/deleteReview",(res,req)=>{
+
+});
+
+//will need a way to edit the review
+app.put("/editReview",(res, req)=>{
+
+});
+
+//will need a way to get all reviews
+app.get("/getReviews", (res, req)={
+    
+});
 
 module.exports = {
     app
