262a51887843fec4f6620bc2f7ced4f61090a850
Kaleb Petersen <kcpetersen111@gmail.com>
2022-06-24 11:57:36 -0600

    added the api endpoints

--- server.js
+++ server.js
@@ -1,29 +1,67 @@
 const express = require('express');
+const Review = require('./persist/review');
 const app = express();
 
+app.use(express.json())
+
+app.use(express.static(`${__dirname}/public`));
+
 //will need a way to get companies from the db and send it to the front end
-app.get("/getReview",(res,req)=>{
+app.get("/getReview",(req,res)=>{
+    res.json(["Parks Pass","tcn","Stephen Wade","Zonos"]);
 
 });
 
 //will need a way to post a review of the lunch
-app.post("/postReview",(res,req)=>{
+app.post("/postReview",(req,res)=>{
 
+    Review.create(req.body).then((review)=>{
+        console.log(req.body);
+        console.log(review);
+        res.json(review);
+    }).catch((err) =>{
+        res.status(500).json(err);
+    });
 });
 
 //will need a way to delete reviews
-app.delete("/deleteReview",(res,req)=>{
+app.delete("/deleteReview/:id",(req,res)=>{
+    const id = req.params.id
 
+    Review.findByIdAndDelete(id).then(review=>{
+        if(review == null){
+            res.status(404).json({message:"Not found"});
+            return;
+        }
+        res.json(review);
+    }).catch(err =>{
+        res.status(500).json(err);
+    });
 });
 
 //will need a way to edit the review
-app.put("/editReview",(res, req)=>{
+app.put("/editReview/:id",(req,res)=>{
+    const id = req.params.id;
 
+    Review.findByIdAndUpdate(id, req.body,{returnDocument:'after'}).then(review=>{
+        // console.log(review);
+        if(review == null){
+            res.status(404).json({message:"not found"});
+            return;
+        }
+        res.json(review);
+    }).catch(err=>{
+        res.status(500).json(err);
+    });
 });
 
 //will need a way to get all reviews
-app.get("/getReviews", (res, req)=>{
-    
+app.get("/getReviews", (req,res)=>{
+    Review.find().then(reviews =>{
+        res.json(reviews);
+    }).catch(err=>{
+        res.status(500).json(err);
+    })
 });
 
 module.exports = app;
