9fb7a6f68edc6a7450809abd66cabdf72211181f
Kaleb Petersen <kcpetersen111@gmail.com>
2022-06-24 18:22:14 -0600

    Continuing the work on the front end

--- public/app.js
+++ public/app.js
@@ -9,6 +9,31 @@ var app = new Vue({
       companies:[],  
       reviews: [],
       currentlyEditing: -1,
+      tempRating:0,
+      tempCompany:"",
+      tempReview:"",
+    },
+    methods:{
+      updateButtonClick: function(index){
+        this.currentlyEditing = index;
+        console.log("update button press"+index);
+      },
+      deleteButtonClick: function(index){
+        console.log("delete button press"+index);
+      },
+      addReview: function(){
+        console.log("create a new review");
+      },
+      submitButton: function(review){
+        console.log("submit button click");
+        review.review = this.tempReview;
+        review.rating = this.tempRating;
+        review.companyName = this.tempCompany;
+        currentlyEditing = -1;
+      },
+      cancelButton: function(index){
+        console.log("cancel button click");
+      },
     },
 
     created: function() {
