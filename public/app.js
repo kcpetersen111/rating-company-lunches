a0280e4fd9c835d4ee3a6ed7c38fef7fedf3ce9c
Kaleb Petersen <kcpetersen111@gmail.com>
2022-06-26 16:43:05 -0600

    adding a frontend will need to make look better later

--- public/app.js
+++ public/app.js
@@ -1,3 +1,6 @@
+// need to find what plugin keeps adding this code
+// const { response } = require("../server");
+
 // const { response } = require("../server");
 
 const url="http://localhost:8080";
@@ -9,41 +12,128 @@ var app = new Vue({
       companies:[],  
       reviews: [],
       currentlyEditing: -1,
-      tempRating:0,
+      tempRating:"",
       tempCompany:"",
       tempReview:"",
+      currentlyAdding:false,
+      addingCompany:"",
+      addingRating:"",
+      addingReview:"",
     },
     methods:{
       updateButtonClick: function(index){
         this.currentlyEditing = index;
         console.log("update button press"+index);
       },
-      deleteButtonClick: function(index){
-        console.log("delete button press"+index);
+      deleteButtonClick: function(review){
+        console.log("delete button press");
+        console.log(review);
+        const id = review._id;
+
+        fetch(url+"/deleteReview/"+id, {
+          method: "DELETE"
+        }).then(response=>{
+          response.json().then(deleted=>{
+            console.log(deleted);
+            //probably refresh the list of reviews  
+            this.refreshReviews()
+          })
+        })
+
       },
       addReview: function(){
-        console.log("create a new review");
+        this.currentlyAdding = true;
       },
+      attemptToPostToServer(){
+        if(this.addingCompany == "" || this.addingRating=="" || this.addingReview=="" ||this.addingRating>10 ||this.addingRating<1 ||!this.companies.includes(this.addingCompany)){
+          return;
+        }
+        this.submitAddReview();
+      },
+      submitAddReview: function(){
+        //fill in the review
+        let review = {
+          name: "",
+          companyName: this.addingCompany,
+          review: this.addingReview,
+          rating: this.addingRating
+        };
+        fetch(url+"/postReview",{
+          method:"POST",
+          body: JSON.stringify(review),
+          headers:{
+            "Content-Type": "application/json"
+          }
+        }).then(response=>{
+          response.json().then(data=>{
+            console.log(data)
+            this.refreshReviews();
+          });
+        });
+
+        this.addingCompany = "";
+        this.addingReview = "";
+        this.addingRating = "";
+        this.currentlyAdding = false;
+
+      },
+
       submitButton: function(review){
         console.log("submit button click");
-        review.review = this.tempReview;
-        review.rating = this.tempRating;
-        review.companyName = this.tempCompany;
-        currentlyEditing = -1;
+
+        if(this.tempReview != ""){
+          review.review = this.tempReview;
+        }
+        if(this.tempRating != ""){
+          review.rating = this.tempRating;
+        }
+        if(this.tempCompany != ""){
+          review.companyName = this.tempCompany;
+        }
+
+        fetch(url+"/editReview/"+review._id, {
+          method: "PUT",
+          body: JSON.stringify(review),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then(response =>{
+          response.json().then(data=>{
+            console.log(data)
+            this.refreshReviews;
+          });
+        });
+
+        
+        this.currentlyEditing = -1;
+        this.tempRating = "";
+        this.tempReview = "";
+        this.tempCompany = "";
       },
+
       cancelButton: function(index){
         console.log("cancel button click");
+
+        this.currentlyEditing = -1;
+        this.tempRating = "";
+        this.tempReview = "";
+        this.tempCompany = "";
+
       },
+      refreshReviews: function(){
+        fetch(url+"/getReviews").then(response =>{
+          response.json().then(allReviews=>{
+            this.reviews = allReviews;
+            console.log(allReviews);
+          });
+        });
+      }
+
     },
 
     created: function() {
 
-      fetch(url+"/getReviews").then(response =>{
-        response.json().then(allReviews=>{
-          this.reviews = allReviews;
-          console.log(allReviews);
-        });
-      });
+      this.refreshReviews();
 
       fetch(url+"/getReview").then(response => {
         response.json().then(companies=>{
